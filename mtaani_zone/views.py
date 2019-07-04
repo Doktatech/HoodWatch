d7a50016532af4f1d929f1e16ea1342a6ce8be9b
Doktatech <Doktatech2@gmail.com>
2019-07-04 11:20:43 +0300

    Add the first view function for our app

--- mtaani_zone/views.py
+++ mtaani_zone/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+
+
 
 # Create your views here.
+def landing(request):
+    return HttpResponse('Welcome to our Neighborhood')
