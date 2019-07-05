2010833038bfff64e11489e662c7fd9c90c41918
Doktatech <Doktatech2@gmail.com>
2019-07-05 08:16:06 +0300

    Create the test instance for the notices

--- mtaani_zone/tests.py
+++ mtaani_zone/tests.py
@@ -1,10 +1,23 @@
 from django.test import TestCase
-from .models import Business
+from .models import Business,Notices
 
 
 # Create your tests here.
-class BusinessTest(TestCase):
+class BusinessTestClass(TestCase):
     def setUp(self):
-        self.business=Business(name='McDonalds',address='17TH STREET',business_type='Fast_Foods',description='We offer fast foods')
+        self.business= Business(name='McDonalds',address='17TH STREET',business_type='Fast_Foods',description='We offer fast foods')
     def test_instance(self):
         self.assertTrue(isinstance(self.business,Business))
+    def test_save_method(self):
+        self.business.save_business()
+        business= Business.objects.all()
+        self.assertTrue(len(business)>0)
+class NoticesTestClass(TestCase):
+    def setUp(self):
+        self.notices= Notices(title='Nyumba Kumi',details='Monthly meeting',status='Urgent')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.notices,Notices))
+    def test_save_method(self):
+        self.notices.save_notices()
+        notices=Notices.objects.all()
+        self.assertTrue(len(notices)>0)
