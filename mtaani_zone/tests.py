dca5f0fcd7276a782c66f7d6444a1df235ad42a5
Doktatech <Doktatech2@gmail.com>
2019-07-05 06:54:20 +0300

    Create first test for the business

--- mtaani_zone/tests.py
+++ mtaani_zone/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Business
+
 
 # Create your tests here.
+class BusinessTest(TestCase):
+    def setUp(self):
+        self.business=Business(name='McDonalds',address='17TH STREET',business_type='Fast_Foods',description='We offer fast foods')
+    def test_instance(self):
+        self.assertTrue(isinstance(self.business,Business))
