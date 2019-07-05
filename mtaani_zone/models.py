baaa61036aa737d9ef926c1ea5febef29348477e
Doktatech <Doktatech2@gmail.com>
2019-07-05 08:15:44 +0300

    Create Notices model for notifications

--- mtaani_zone/models.py
+++ mtaani_zone/models.py
@@ -10,16 +10,16 @@ class Business(models.Model):
         return self.name
     def save_business(self):
         self.save()
-# class Notices(models.Model):
-#     Status_Choices=(
-#         (1,'Urgent'),
-#         (2,'Necessary'),
-#         (3,'Unessential'),
-#     )
-#     title=models.CharField(max_length=50)
-#     details=models.TextField()
-#     status= models.CharField(max_length=50, choices=Status_Choices,default='1')
-#     def __str__(self):
-#         return self.title
-#     def save_notices(self):
-#         self.save()
+class Notices(models.Model):
+    Status_Choices=(
+        (1,'Urgent'),
+        (2,'Necessary'),
+        (3,'Unessential'),
+    )
+    title=models.CharField(max_length=50)
+    details=models.TextField()
+    status= models.CharField(max_length=50, choices=Status_Choices,default='1')
+    def __str__(self):
+        return self.title
+    def save_notices(self):
+        self.save()
