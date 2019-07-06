66928f8ee061ec9ccd9fa4f5e6a82a5b31a238ab
Doktatech <Doktatech2@gmail.com>
2019-07-06 12:58:09 +0300

    Create method for geting business objects

--- mtaani_zone/models.py
+++ mtaani_zone/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 class Business(models.Model):
@@ -6,19 +7,29 @@ class Business(models.Model):
     address=models.CharField(max_length=50)
     business_type=models.CharField(max_length=30)
     description=models.TextField()
+    business_image = models.ImageField(upload_to = 'photos/')
     def __str__(self):
         return self.name
     def save_business(self):
         self.save()
+    @classmethod
+    def get_business(cls):
+        business=cls.objects.all()
+        return business
+    @classmethod
+    def search_by_type(cls,search_term):
+        business = cls.objects.filter(business_type__icontains=search_term)
+        return business
+
 class Notices(models.Model):
     Status_Choices=(
-        (1,'Urgent'),
-        (2,'Necessary'),
-        (3,'Unessential'),
+        ('1','Urgent'),
+        ('2','Necessary'),
+        ('3','Unessential'),
     )
     title=models.CharField(max_length=50)
     details=models.TextField()
-    status= models.CharField(max_length=50, choices=Status_Choices,default='1')
+    status= models.CharField(max_length=50, choices=Status_Choices,default='None')
     pub_date = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.title
@@ -26,4 +37,9 @@ class Notices(models.Model):
         self.save()
     class Meta:
         ordering =['status']
+    @classmethod
+    def get_notices(cls):
+        today = dt.date.today()
+        notices=cls.objects.filter(pub_date__date=today)
+        return notices
     
