d230bc8e6215c3ea958681f75938732c098d71ea
Doktatech <Doktatech2@gmail.com>
2019-07-06 17:33:54 +0300

    Create a user Profile class

--- mtaani_zone/models.py
+++ mtaani_zone/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import datetime as dt
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Business(models.Model):
@@ -42,4 +43,28 @@ class Notices(models.Model):
         today = dt.date.today()
         notices=cls.objects.filter(pub_date__date=today)
         return notices
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    bio = models.CharField(max_length=200)
+    profile_pic = models.ImageField(upload_to='profile/')
     
+    def __str__(self):
+        return self.first_name
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profiles(cls):
+        profiles = cls.objects.all()
+        return profiles
+    
+    @classmethod
+    def search_by_username(cls,search_term):
+        profiles = cls.objects.filter(title__icontains=search_term)
+        return profiles
