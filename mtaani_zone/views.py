56f01cdecdbf2730a0894bbfcfe2a783981d864f
Doktatech <Doktatech2@gmail.com>
2019-07-06 17:09:38 +0300

    Add view function for a single business

--- mtaani_zone/views.py
+++ mtaani_zone/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 import datetime as dt
-
+from .models import Business, Notices
 
 
 # Create your views here.
-def landing(request):    
-    return render(request, 'landing.html',)
+def landing(request): 
+    business=Business.get_business()   
+    return render(request, 'landing.html',{"business":business})
+def search_results(request):
+    if 'business' in request.GET and request.GET["business"]:
+        search_term =request.GET.get("business")
+        searched_business = Business.search_by_type(search_term)
+        message=f"{search_term}"
+        return render(request, 'search.html',{"message":message,"businesses": searched_business})
+    else:
+        message = "You haven't searched for any Business"
+        return render(request, 'search.html',{"message":message})
+def business(request,business_id):
+    try:
+        business= Business.objects.get(id=business_id)
+    except ValueError:
+        return render(request,"single_article.html","business":business)
