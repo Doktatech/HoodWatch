9c81a1768383427be5cfcaa89e14571254062a7d
Doktatech <Doktatech2@gmail.com>
2019-07-07 07:44:15 +0300

    Re-register the models

--- mtaani_zone/admin.py
+++ mtaani_zone/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Notices, Business,Profile
+from .models import Notices, Business,Profile,Neighborhood
 
 # Register your models here.
 admin.site.register(Business)
 admin.site.register(Notices)
 admin.site.register(Profile)
+admin.site.register(Neighborhood)
