674b25d7249e870fa15b147989cb690a4741bb8b
Doktatech <Doktatech2@gmail.com>
2019-07-07 16:14:40 +0300

    Deployment to github

--- mtaani_zone/urls.py
+++ mtaani_zone/urls.py
@@ -16,6 +16,8 @@ urlpatterns=[
     url(r'^hoods/new/business/(\d+)$',views.post_business, name='new-business'),
     url(r'^hoods/(\d+)',views.hoods,name='hoods'),
     url(r'^signup/$', views.signup, name='signup'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
     
 ]
 if settings.DEBUG:
