68fdc65bbfee15cf560226e19d3ce2ff51c588e7
Doktatech <Doktatech2@gmail.com>
2019-07-07 14:45:54 +0300

    Add url pattern for the signup

--- mtaani_zone/urls.py
+++ mtaani_zone/urls.py
@@ -15,6 +15,7 @@ urlpatterns=[
     # url(r'^map$', views.maps, name='maps'),
     url(r'^hoods/new/business/(\d+)$',views.post_business, name='new-business'),
     url(r'^hoods/(\d+)',views.hoods,name='hoods'),
+    url(r'^signup/$', views.signup, name='signup'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
