1638f8ba6cdc4f916496ee5c538decd4870c8799
Doktatech <Doktatech2@gmail.com>
2019-07-07 05:47:48 +0300

    Connect user profile model to admin

--- mtaani_zone/admin.py
+++ mtaani_zone/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Notices, Business
+from .models import Notices, Business,Profile
 
 # Register your models here.
 admin.site.register(Business)
 admin.site.register(Notices)
+admin.site.register(Profile)
